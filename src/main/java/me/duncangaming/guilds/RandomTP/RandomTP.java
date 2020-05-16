92b46a2081d51c0762d604045a11eea51c2d7783
soccercraft <wijnbergduncan@gmail.com>
2020-05-15 21:13:20 -0700

    Added RandomTP

---
+++ src/main/java/me/duncangaming/guilds/RandomTP/RandomTP.java
@@ -0,0 +1,15 @@
+package me.duncangaming.guilds.RandomTP;
+
+import java.util.Random;
+
+public class RandomTP {
+
+    public static void randomNumber(String[] args) {
+        Random dice = new Random();
+        int x;
+        int z;
+        x = dice.nextInt(500000);
+        z = dice.nextInt(500000);
+    }
+
+}
