2c0bb700de6b12b9c23e2dd562f1387cabed49cb
soccercraft <wijnbergduncan@gmail.com>
2020-05-15 20:59:19 -0700

    Initial commit

---
+++ src/main/java/me/duncangaming/guilds/Guilds.java
@@ -0,0 +1,17 @@
+package me.duncangaming.guilds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Guilds extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
